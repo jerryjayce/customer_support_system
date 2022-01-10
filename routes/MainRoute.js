9141a0c95e4e2fc1faad538d71e792edaf81ded9
jerryjayce <jerryjayce@gmail.com>
2022-01-10 11:29:31 +0100

    database deployed

--- routes/MainRoute.js
+++ routes/MainRoute.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const router = express.Router();
 
-const MainController = require('../controllers/MainController');
 const IssuesController = require('../controllers/Issues');
 
 //issues
