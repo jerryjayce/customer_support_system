9f114726d96310debe031d9e7d31eb38265222ef
jerryjayce <jerryjayce@gmail.com>
2022-01-10 13:26:50 +0100

    data validators(create issue, close issue)

--- routes/MainRoute.js
+++ routes/MainRoute.js
@@ -1,15 +1,15 @@
-
 const express = require("express");
 const router = express.Router();
 
+//data validators
+let {create_issue, close_issue} = require("../middlewares/formValidator");
+
+//controllers
 const IssuesController = require('../controllers/Issues');
 
-//issues
-router.post('/issues/create', IssuesController.create_issue);
-router.put('/issues/close', IssuesController.close_issue);
-
-
-//agent
+//issues route
+router.post('/issues/create', [create_issue], IssuesController.create_issue);
+router.put('/issues/close', [close_issue], IssuesController.close_issue);
 
 
 module.exports = router;
