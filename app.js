d49e5786ac3eec18a0ef3e1bb5a6d52313776f12
jerryjayce <jerryjayce@gmail.com>
2022-01-10 10:58:21 +0100

    body paser for form data

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
 let MainRoute =  require('./routes/MainRoute');
 let sendResponse = require('./helpers/ResponseHelper').sendResponse;
 
@@ -11,6 +13,10 @@ let sendResponse = require('./helpers/ResponseHelper').sendResponse;
 app.use(cors());
 app.options('*', cors());
 
+// MiddleWares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(helmet());
 
 // starting the app
 const port = 9900;
